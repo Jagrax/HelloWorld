bd640d69ec9f9304ea5dea58ff9a2b571cb6efd5
Javier Alejandro Giani <jagrax@hotmail.com>
2015-08-01 20:22:20 -0300

    Print a message
    Signed-off-by: Javier Alejandro Giani <jagrax@hotmail.com>

--- src/ar/com/helloworld/Hello.java
+++ src/ar/com/helloworld/Hello.java
@@ -2,4 +2,12 @@ package ar.com.helloworld;
 
 public class Hello {
 
+	/**
+	 * Implement the application
+	 * 
+	 * @param args arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
 }
